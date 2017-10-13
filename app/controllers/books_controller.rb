3aab0a64e5432b4873553e718096dd7ee4cf9320
Anna <anna@thenewsavvy.com>
2017-10-13 15:25:27 +0800

    Reading List

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    Book.create(book_params)
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def book_params
+    params.require(:book).permit(:name, :author)
+  end
+
+end
---
+++ db/migrate/20171012083335_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :name, null: false
+      t.string :author, null: false
+      t.timestamps
+    end
+  end
+end
